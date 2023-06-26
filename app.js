58ba0e935a82d8085ef69f94927101fd27ed8548
Roshan <shahroshan0720@gmail.com>
2023-06-26 19:05:28 +0530

    connection of database

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 require('dotenv').config()
+require('express-async-errors');
+
 const express = require("express");
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("hellow world");
-});
+const connectDB = require('./db/connect');
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
+
+app.use('api/v1/auth',authRouter);
+app.use('api/v1/jobs',jobsRouter);
 
 const port = process.env.PORT || 3000;
 
 const start =  async()=>{
     try{
+        await connectDB(process.env.MONGO_URI);
         app.listen(port,console.log(`server connected at http://localhost:${port}`));
     }catch(error){
         console.log(error);
