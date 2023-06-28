61e4a7fe3add48dc573382fa766d939f69a501c5
Roshan <shahroshan0720@gmail.com>
2023-06-28 10:52:31 +0530

    setup update and delete api

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -29,11 +29,38 @@ const createJob = async (req,res) =>{
 };
 
 const updateJob = async (req,res) =>{
-    res.send("update job");
+    const {
+        body:{company, position},
+        user: {userId},
+        params: {id: jobId}    
+    } = req;
+
+    if(company === '' || position === ''){
+        throw new BadRequestError('Company or Position fields cannot be Empty');
+    }
+    const job = await Job.findByIdAndUpdate(
+        {_id: jobId, createdBy: userId},
+        req.body,
+        {new: true, runValidators: true}
+    )
+
+    if(!job){
+        throw NotFoundError(`No job fount with id${jobId}`);
+    }
+    res.status(StatusCodes.OK).json({job});
 };
 
 const deleteJob = async (req,res) =>{
-    res.send("delete job");
+    const {user:{userId},params:{id:jobId}} = req;
+
+    const job = await Job.findByIdAndRemove({
+        _id : jobId,
+        createdBy: userId
+    })
+    if(!job){
+        throw new NotFoundError(`No job with id ${jobid}`);
+    }
+    res.status(StatusCodes.OK).send();
 };
 
 
